b29e437093de17c2d015f60ac4b06ff175a47b95
RoachLok <jtabordajunior@hotmail.com>
2019-11-28 00:53:04 +0100

    Chat Server first commit. General progress: - Working UID. - Ables for client - server - client communications - TODOS: JSON implementation for messages database. This will allow for chat history and mark messages as read.

---
+++ src/chat_server/Main.java
@@ -0,0 +1,201 @@
+package chat_server;
+
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.URL;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.ResourceBundle;
+
+public class Main extends Application implements Runnable, Initializable {
+
+    private Thread thread;
+    private ServerSocket serverSocket;
+    private int serverPort = 56789;
+    private InetAddress inetAddress;
+    private PrintWriter writer;
+
+    private DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss");
+    private LocalDateTime currentTime;
+
+    private ArrayList<Client> clientList;
+    private ArrayList<String> messageList;
+
+    @FXML
+    private Button startServer;
+    @FXML
+    private Button stopServer;
+    @FXML
+    TextArea textArea;
+    @FXML
+    private TextField portField;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        portField.disableProperty().bind(startServer.disabledProperty());
+        stopServer.disableProperty().bind(startServer.disabledProperty().not());
+
+        startServer.setOnAction((event) -> {  //https://stackoverflow.com/questions/33986917/javafx-setonaction-not-applicable
+            serverPort = Integer.parseInt(portField.getText());
+            if (serverPort < 65536)
+                serverStart();
+            else {
+                currentTime = LocalDateTime.now();
+                textArea.appendText("\n["+dtf.format(currentTime)+']'+" ERROR. Invalid port value. Port valid range: 0 - 65535.");
+            }
+        });
+        stopServer.setOnAction((event) -> {
+            serverStop();
+        });
+
+        textArea.setWrapText(true);
+        textArea.setEditable(false);
+        currentTime = LocalDateTime.now();
+        textArea.setText('['+dtf.format(currentTime)+']'+" READY. Waiting for user action.");
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("ServerView.fxml"));
+        primaryStage.setTitle("SafeJourney Chat Server");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
+    }
+
+    @Override
+    public void run() {
+        while(thread != null){
+            try{
+                Socket socket = serverSocket.accept();
+                new ServerClientHandler (this, socket);
+                Platform.runLater(() -> {
+                    textArea.appendText("\n["+dtf.format(currentTime)+']'+" INFO. New Client Connected.");
+                });
+                Thread.sleep(200);
+            }catch(Exception e){
+                e.printStackTrace();
+                serverStop();
+            }
+        }
+    }
+
+    private void serverStart(){
+        try {
+            serverSocket = new ServerSocket(serverPort);
+            inetAddress = InetAddress.getLocalHost();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        clientList = new ArrayList<Client>();
+        messageList = new ArrayList<>();
+
+        thread = new Thread(this);
+        thread.start();
+
+        //Disable "Start Server" button.
+        startServer.setDisable(true);
+        currentTime = LocalDateTime.now();
+        textArea.setText('['+dtf.format(currentTime)+']'+" RUNNING. Server launched in localhost: "+inetAddress.getHostAddress()+ ". Waiting for client.");
+    }
+
+    private void serverStop() {
+        if (serverSocket != null) {
+            for (Client client : clientList) sendMessage(client.getSocket(), "FIN. SERVER CLOSED.");
+            thread = null;
+            try {
+                serverSocket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                clientList = null;
+                messageList = null;
+            }
+            currentTime = LocalDateTime.now();
+            textArea.appendText("\n["+dtf.format(currentTime)+']'+" STOPPED. Server stopped by user action.");
+            //Enable "Start Server" button
+            startServer.setDisable(false);
+        }
+    }
+
+    void sendMessage(Socket clientSocket, String message) {
+        if (clientSocket != null && !clientSocket.isClosed()) {                                                     //User disconnected/doesn't exist?
+            String[] parsedField = message.split("><");                                         //Message example: MSG><FROM><FOR><MSGCONTENT
+                                                                                                                    //parse[0] = from; parse[1] = for; parse[2] = msg;
+            for (Client destinatary : clientList) {
+                if (destinatary.getUsername().equals(parsedField[1])) {                                             //Client "online".
+                    try {
+                        writer = new PrintWriter(destinatary.getSocket().getOutputStream(), true);
+                        writer.println("MSG><"+parsedField[0]+"><"+parsedField[2]);
+                        break;
+                    } catch (IOException ex) {
+                        ex.printStackTrace();
+                        writer.close();
+                    }
+                }
+            }
+            //Print log in server
+            currentTime = LocalDateTime.now();
+            textArea.appendText("\n["+dtf.format(currentTime)+']'+" INFO. User -> "+parsedField[0]+" sent message '"+parsedField[2]+"' to -> "+ parsedField[1]+".");
+            //Store message for message history.
+            storeMessage(parsedField[0], parsedField[1], parsedField[2]);
+        }
+    }
+
+    private void storeMessage(String sender, String destinatary, String messageContent) { //TODO maybe add timestamps to messages too.
+        /*** IMPLEMENT JSON ***/
+        // All stored messages should be marked as not read.
+        currentTime = LocalDateTime.now();
+        textArea.appendText("\n["+dtf.format(currentTime)+']'+" INFO. "+sender+"'s message to -> "+destinatary+" was stored.");
+    }
+
+    void markAsRead(String reciever){
+        /*** Mark reviever messages as read ***/
+        currentTime = LocalDateTime.now();
+        textArea.appendText("\n["+dtf.format(currentTime)+']'+" INFO. "+reciever+" read his message.");
+    }
+
+    void addClient(Socket socket, String username, String adminUsername) {
+        for (Client client : clientList) { //Checks if client is already in
+            if (client.getUsername().equals(username))
+                return;
+        }
+        if (adminUsername.charAt(0) == '.') {
+            clientList.add(new ClientSupervisor(socket, username));
+            return;
+        }
+        clientList.add(new ClientDriver(socket, username, adminUsername));
+    }
+
+    void removeClient(Socket socket, String username) {
+        for (int i = 0; i < clientList.size(); i++){
+            if (clientList.get(i).getUsername().equals(username)) {
+                textArea.appendText("\nUser "+clientList.get(i).getUsername()+" disconnected. ");
+                clientList.remove(i);
+                textArea.appendText('('+clientList.size()+')'+" users left.");
+                return;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+
+}
---
+++ src/chat_server/ServerClientHandler.java
@@ -0,0 +1,99 @@
+package chat_server;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class ServerClientHandler implements Runnable {
+    private Main server;
+    private Socket socket;
+    private Thread thread;
+    private BufferedReader dataIn;
+    private PrintWriter dataOut;
+    private String text;
+    private String username;
+
+
+    ServerClientHandler(Main server, Socket clientSocket) {
+        this.server = server;
+        this.socket = clientSocket;
+        try {
+            dataIn = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            dataOut = new PrintWriter(clientSocket.getOutputStream(), true);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        thread = new Thread(this);
+        thread.start();
+    }
+
+    @Override
+    public void run() {
+        int timeoutCounter = 0;
+        while (thread != null) {
+            try {
+                String textIn = dataIn.readLine();        //TODO Change for switch.
+                if (textIn.startsWith("SYN")) {
+                    if (timeoutCounter < 3) {
+                        timeoutCounter++;
+                        dataOut.println("SYN-ACK><Hey there!");
+                    }else {
+                        server.sendMessage(socket, "FIN. USER TIMED-OUT");
+                        disconnectUser();
+                    }
+                } else if (textIn.startsWith("ACKMSG")){                                                        //Message was read.
+                    server.markAsRead(textIn.substring(6));
+
+                } else if (textIn.startsWith("ACK")) {                                                          //Client has handshaked
+                                                                                                                //Sample admin message: ACKMyname><.     Notice admin's admin is .
+                                                                                                                //Sample driver message: ACKMyname><MyAdminName
+                    String[] parsedField = textIn.substring(5).split("><");
+                    server.addClient(socket, parsedField[0], parsedField[1]);
+
+                } else if (textIn.startsWith("MSG")) {                                                          //User sending message to user.
+                    server.sendMessage(socket, textIn.substring(5));
+
+                } else if (textIn.startsWith("FIN"))                                                            //User disconnecting.
+                    disconnectUser();
+
+            } catch (IOException e) {
+                e.printStackTrace();
+                disconnectUser();
+            }
+        }
+
+        //server.textArea.appendText("\nCommunication stopped for Client:(" + userName + ")");
+    }
+
+    public void disconnectUser() {
+        server.textArea.appendText("\nDisconnecting Client: (" + username + ")...");
+        server.removeClient(socket, username);
+        stopConnection();
+    }
+
+    public void stopConnection() {
+        thread = null;
+
+        // Close toClient
+        dataOut.close();
+
+        // Close fromClient
+        try {
+            dataIn.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        // Close the socket
+        try {
+            socket.close();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+
+    }
+}
+
+
