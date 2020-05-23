ec2eb4a93b7aa9473dd7100f03a7a03aafedb2e3
RoachLok <jtabordajunior@hotmail.com>
2020-05-24 01:06:26 +0200

    Changed "Base URL" to new URL.

--- src/pkg2ingproyi/Util/APIRoutes.java
+++ src/pkg2ingproyi/Util/APIRoutes.java
@@ -3,7 +3,7 @@ package pkg2ingproyi.Util;
 import pkg2ingproyi.Main;
 
 public class APIRoutes {
-    private final static String baseUrl     = "https://JJ82BM9WO382QD8-SJ.adb.eu-amsterdam-1.oraclecloudapps.com/ords/admin/";
+    private final static String baseUrl     = "https://jj82bm9wo382qd8-sj3.adb.eu-amsterdam-1.oraclecloudapps.com/ords/admin/";
     private final static String dptQuery    = "/?q={%22department_id%22:%22"+ Main.appUser.getDptId() +"%22}";
 
     public final static String CHAUFFEUR    = baseUrl + "chauffeur" + dptQuery;
