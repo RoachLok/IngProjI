ab40df9e335529ed391e921703d5618b93551035
RoachLok <jtabordajunior@hotmail.com>
2020-05-23 23:56:30 +0200

    Added static for URL constants.

--- src/pkg2ingproyi/Util/APIRoutes.java
+++ src/pkg2ingproyi/Util/APIRoutes.java
@@ -1,4 +1,13 @@
 package pkg2ingproyi.Util;
 
-public enum APIRoutes {
+import pkg2ingproyi.Main;
+
+public class APIRoutes {
+    private final static String baseUrl     = "https://JJ82BM9WO382QD8-SJ.adb.eu-amsterdam-1.oraclecloudapps.com/ords/admin/";
+    private final static String dptQuery    = "/?q={%22department_id%22:%22"+ Main.appUser.getDptId() +"%22}";
+
+    public final static String CHAUFFEUR    = baseUrl + "chauffeur" + dptQuery;
+    public final static String VEHICLES     = baseUrl + "vehicle"   + dptQuery;
+    public final static String SERVICES     = baseUrl + "service"   + dptQuery;
+    public final static String ACCOUNTANT   = baseUrl + "accountant"+ dptQuery;
 }
