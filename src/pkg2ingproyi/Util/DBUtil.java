84da901b57e0cb4d2e1e09b30fc1b15126a6ed7d
RoachLok <jtabordajunior@hotmail.com>
2020-05-28 01:34:42 +0200

    Added fixDateFormat to fix date format into a comprehensible one.

--- src/pkg2ingproyi/Util/DBUtil.java
+++ src/pkg2ingproyi/Util/DBUtil.java
@@ -1,4 +1,12 @@
 package pkg2ingproyi.Util;
 
 public class DBUtil {
+
+    public static String fixDateFormat(String dbDateStr) {
+        dbDateStr = dbDateStr.replace('-', '/');
+        dbDateStr = dbDateStr.replace('T', ' ');
+        dbDateStr = dbDateStr.substring(0, 19);
+        return dbDateStr;
+    }
+
 }
