254e9d27744c61ede93e9aa3bb5e7ead5c8ee96f
RoachLok <jtabordajunior@hotmail.com>
2020-05-31 01:41:35 +0200

    NetworkDELETERequester added. Allows to do DELETE Requests to the DB API.

--- src/pkg2ingproyi/Util/NetworkDELETERequester.java
+++ src/pkg2ingproyi/Util/NetworkDELETERequester.java
@@ -1,4 +1,37 @@
 package pkg2ingproyi.Util;
 
-public class NetworkDELETERequester {
+import javafx.application.Platform;
+import org.controlsfx.control.Notifications;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class NetworkDELETERequester extends Thread {
+
+    private final String strUrl, objectId;
+    private final ConnectionChecker conChecker;
+
+    public NetworkDELETERequester(String strUrl, String objectId, ConnectionChecker conChecker) {
+        this.strUrl     = strUrl;
+        this.objectId   = objectId;
+        this.conChecker = conChecker;
+    }
+
+    @Override
+    public void run() {
+        try {
+            URL url = new URL(strUrl + '/' + objectId);
+            HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+            urlConnection.setRequestMethod("DELETE");
+            urlConnection.connect();
+
+            //Check if connection was stabilised correctly.
+            if (urlConnection.getResponseCode() != 200)
+                conChecker.onDataFail();
+            else {
+                Platform.runLater(() -> Notifications.create().title("Element Deleted").text("El elemento con ID: '" + objectId + "' fue eliminado de la BD.").showConfirm());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
