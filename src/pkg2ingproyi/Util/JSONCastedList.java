af5d6422791501878d0816a1d1f18b0f2452ff3a
RoachLok <jtabordajunior@hotmail.com>
2020-05-31 01:23:11 +0200

    Changes made to allow for easier NetworkGETRequester usage. The jsonString is now internally parsed. \nChanges made to SupervisorViewControler.java to adapt for this change.

--- src/pkg2ingproyi/Util/JSONCastedList.java
+++ src/pkg2ingproyi/Util/JSONCastedList.java
@@ -2,14 +2,18 @@ package pkg2ingproyi.Util;
 
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
 
 import java.util.ArrayList;
 
 public class JSONCastedList<M extends JSONSerializable> extends ArrayList<M> {
 
-    public JSONCastedList(JSONArray jsonArray, Class<M> className) throws Exception {
+    public JSONCastedList(String rawData, Class<M> className) throws Exception {
+        JSONParser  jsonParser  = new JSONParser();
+        JSONObject  jsonObject  = (JSONObject) jsonParser.parse(rawData);
+        JSONArray   jsonArray   = (JSONArray ) jsonObject.get("items");
+
         for (Object o : jsonArray)
             this.add(ModelFactory.buildModel(className, (JSONObject) o));
-            //className.getDeclaredConstructor(JSONObject.class).newInstance(o));
     }
 }
