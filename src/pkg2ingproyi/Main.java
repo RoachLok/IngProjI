d9188465ecd58d6e4cead45365dd434dfd1c9dda
RoachLok <jtabordajunior@hotmail.com>
2020-06-03 16:33:36 +0200

    Fixed route to allow the .jar grab the icon_logo.png.

--- src/pkg2ingproyi/Main.java
+++ src/pkg2ingproyi/Main.java
@@ -25,7 +25,7 @@ public class Main extends Application {
 
         Scene scene = new Scene(root);
         primaryStage = new Stage();
-        primaryStage.getIcons().add(new Image("./resources/icons/icon_logo.png"));
+        primaryStage.getIcons().add(new Image("/resources/icons/icon_logo.png"));
         primaryStage.setScene     (scene);
         primaryStage.setResizable (false);
         primaryStage.sizeToScene    ();
